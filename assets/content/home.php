ef5344996075853850297e400c0d6a3cbc5902dc
Agrin Fauzi <agrinfauzi@gmail.com>
2018-08-25 11:34:19 +0700

    Version 1.2.5

--- assets/content/home.php
+++ assets/content/home.php
@@ -1,10 +1,6 @@
 <div class="col-md-8 ml-auto mr-auto">
   <form id="convert" action="" method="GET">
-<<<<<<< HEAD
-    <div class="brand" style="margin-top: 100px;">
-=======
     <div class="brand">
->>>>>>> 0af30523f0d03c51a84958df26a3347e84898655
       <h1 class="title">
       Paste your <i><small>youtube_id</small></i>
       </h1>
@@ -16,31 +12,4 @@
       <button class="h6 btn btn-neutral btn-md" onclick="videos()" value="Videos">Convert to Videos</button>
     </div>
   </form>
-<<<<<<< HEAD
 </div>
-=======
-</div>
-
-<!-- Start Modal --><!--
-<div class="modal fade" id="mp3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header justify-content-center">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-        <i class="now-ui-icons ui-1_simple-remove"></i>
-        </button>
-        <h4 class="title title-up">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-        </p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default">Nice Button</button>
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div> -->
-<!--  End Modal -->
->>>>>>> 0af30523f0d03c51a84958df26a3347e84898655
